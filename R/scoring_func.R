ada81da21f0b84f5019efd7c732b4613d72374a7
DanWeinberger <56560444+DanWeinberger@users.noreply.github.com>
2023-09-28 16:25:06 -0400

    Update scoring_func.R for scaled data

--- R/scoring_func.R
+++ R/scoring_func.R
@@ -4,19 +4,23 @@ scoring_func <- function(Y){
   
   test1 <-inla.posterior.sample(1000, Y$mod, selection = list('Predictor'=forecast.index), num.threads=NULL)
   
-  log.pred.samples <- sapply(test1, function(X) X$latent , simplify='array')
+  log.scale.pred.samples <- sapply(test1, function(X) X$latent , simplify='array')
   
-  pred.samples <- exp(log.pred.samples) - 1   
+  #pred.samples <- exp(log.pred.samples) - 1   
   
-  obs <- Y$ds$Dengue_fever_rates[forecast.index]
-  
+  #obs <- Y$ds$Dengue_fever_rates[forecast.index]
+   obs <- Y$ds$log_df_scale[forecast.index]
+
   miss.obs <- which(is.na(obs))
   if(length(miss.obs>0)){
     obs <- obs[-miss.obs]
-    pred.samples <- pred.samples[-miss.obs,1,]
+    log.scale.pred.samples  <- log.scale.pred.samples[-miss.obs,1,]
   }
   
-  crps1 <- crps_sample(obs, pred.samples[,1,])
+  crps1 <- crps_sample(obs, log.scale.pred.samples[,1,])
+  
+  return(crps1)
+}
   
   return(crps1)
 }

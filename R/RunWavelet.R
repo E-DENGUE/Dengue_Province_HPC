2519d305c1efdd292da2ea3630ee460da428c549
weinbergerlab <dweinber@gmail.com>
2023-10-19 11:43:58 -0400

    additional cleaning and explortaion

--- R/RunWavelet.R
+++ R/RunWavelet.R
@@ -9,6 +9,7 @@ library(pbapply)
 library(INLA)
 library(MASS)
 library(scoringutils)
+library(epiCleanr)
 
 source('./R/WaveletPkg.R')
 
@@ -23,14 +24,21 @@ source('./R/WaveletPkg.R')
 # 
 # d1 <-  bind_rows(read.xl_files) %>%
 #   rename(date=year_month) 
+
+admin_names <- get_admin_names(country_name_or_code = "Vietnam")
+
 d1 <- read.csv('./Data/historical_input_data_osf_io4zgrx.csv') %>%
   filter(country=='vietnam') %>%
   rename(province=admin1) %>%
-  mutate(date = as.Date(date,'%d/%m/%Y')) %>%
-  filter(date>='2001-01-01' & date<='2017-12-01')
+  mutate(date = as.Date(date,'%d/%m/%Y'),
+         more_than_10 =if_else(total>10,1,0)) %>%
+  group_by(province) %>%
+  mutate(N_months_above_10 =sum(more_than_10, na.rm=T)) %>%
+  filter(date>='2001-01-01' & date<='2017-12-01' & N_months_above_10>10) %>%
+  ungroup()
 
 all.provinces <- unique(d1$province)
-all.provinces <- all.provinces[-grep('tuyen quang',all.provinces)]
+#all.provinces <- all.provinces[-grep('tuyen quang',all.provinces)]
 
 runwaves <- function(X) {
   wave1<- d1 %>% 
@@ -80,15 +88,16 @@ phase.diff <- cbind.data.frame('phase.diff.1x'=mean.phase.diff,'province'=provin
 arrange(-phase.diff.1x)
 
 #Leaing provinces note red river delta region and northeast are adjacent:
-##hung yen (red river delta); 
-#thai nguyen (northeast), 
-#hai duong (red river delta), 
 #bac giang (northeast); 
 #thai binh (red river delta), 
 #dak nong (central highlands), 
-#ha giang (northeast), 
-#vinh phuc (red river delta), 
 #binh phuoc (southeast)
+#phu yen (south central coast)
+#soc trang (MDR)
+
+
+
+
 
 ## OPTION 2: look at start or mid point
 ### Drawbacks--need to be able to define epidemiological year...not clear where boundary is
@@ -124,4 +133,24 @@ peak_delays <- apply(all.phases,2,mean)
   peak_delays <- cbind.data.frame(peak_delays[-1], all.provinces)
 
   ##This tells a similar story to the approach1:
-  #leading provinces ar ein Northeast and red river delta (and maybe northwest)
+  #leading provinces ar ein Northeast and red river delta (and maybe northwest); also some in MDR
+#an giang is in MDR
+  #ba ria vung tau is in southeast
+  #bac lieu is in MDR
+  
+  d1.early <- d1 %>%
+    filter(province %in% c('bac giang',   'thai binh',    'dak nong',  'binh phuoc', 'phu yen','soc trang' , ' ba ria vung tau')) %>%
+    dplyr::select(province, date, total)
+  # ggplot(d1.early, aes(x=date, y=total, group=province, color=province))+geom_line() +facet_wrap(~province, scales='free')
+  
+  
+  #test Gibb data
+  # d1 <- read.csv('https://raw.githubusercontent.com/rorygibb/dengue_vietnam_ms/main/data/dengue/dengue_districts_19982020.csv') %>%
+  #   mutate(province=tolower(province), date=as.Date(date)) %>%
+  #            group_by(province, date) %>%
+  #   summarize( cases=sum(cases, na.rm=T)) %>%
+  #   ungroup() %>%
+  #   filter(date>='2001-01-01' & date<='2017-12-01')
+  # 
+  # ggplot(d1, aes(x=date, y=cases, group=province, color=province))+geom_line() +facet_wrap(~province, scales='free')
+  # 
